6b9f1e3ffa22fa8bd7a0f868a1504900e6463318
root <jessikbs.37@gmail.com>
2023-01-08 09:58:25 +0000

    Add part 1 functions

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -28,6 +28,8 @@ from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_proce
 
 from tools.objdet_models.darknet.models.darknet2pytorch import Darknet as darknet
 from tools.objdet_models.darknet.utils.evaluation_utils import post_processing_v2
+from tools.objdet_models.resnet.utils.torch_utils import _sigmoid
+
 
 
 # load model-related parameters into an edict
@@ -56,12 +58,57 @@ def load_configs_model(model_name='darknet', configs=None):
         configs.num_workers = 4
         configs.pin_memory = True
         configs.use_giou_loss = False
+        
 
     elif model_name == 'fpn_resnet':
         ####### ID_S3_EX1-3 START #######     
         #######
         print("student task ID_S3_EX1-3")
+        
+        configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'resnet')
+        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'fpn_resnet_18_epoch_300.pth')
+            
+        configs.arch = 'fpn_resnet'
+        configs.saved_fn = 'fpn_resnet'
+        configs.K = 50
+        configs.num_samples = None
+        configs.num_workers = 1       
+        configs.batch_size = 1
+        configs.peak_thresh = .2
+        configs.save_test_output = False
+        configs.output_format = 'image'
+        configs.output_video_fn = 'out_fpn_resnet'
+        configs.conf_thresh = 0.5
+        configs.nms_thresh = 0.4
+        configs.use_giou_loss = False
+        configs.pin_memory = True
+        configs.distributed = False  # For testing on 1 GPU only
 
+        configs.input_size = (608, 608)
+        configs.hm_size = (152, 152)
+        configs.down_ratio = 4
+        configs.max_objects = 50
+
+        configs.imagenet_pretrained = False
+        configs.head_conv = 64
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2  # sin, cos
+        
+        configs.heads = {
+        'hm_cen': configs.num_classes,
+        'cen_offset': configs.num_center_offset,
+        'direction': configs.num_direction,
+        'z_coor': configs.num_z,
+        'dim': configs.num_dim
+        }
+            
+        configs.num_input_features = 4
+
+        
+   
         #######
         ####### ID_S3_EX1-3 END #######     
 
@@ -119,6 +166,10 @@ def create_model(configs):
         #######
         print("student task ID_S3_EX1-4")
 
+        num_layers = 18        
+        model = fpn_resnet.get_pose_net(num_layers=num_layers, heads=configs.heads, head_conv=configs.head_conv,
+                                        imagenet_pretrained=configs.imagenet_pretrained)
+        
         #######
         ####### ID_S3_EX1-4 END #######     
     
@@ -167,28 +218,43 @@ def detect_objects(input_bev_maps, model, configs):
             ####### ID_S3_EX1-5 START #######     
             #######
             print("student task ID_S3_EX1-5")
-
-            #######
-            ####### ID_S3_EX1-5 END #######     
-
+            outputs['hm_cen'] = _sigmoid(outputs['hm_cen'])
+            outputs['cen_offset'] = _sigmoid(outputs['cen_offset'])
             
-
+            
+            detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'],
+                                outputs['dim'], K=configs.K)
+            detections = detections.cpu().numpy().astype(np.float32)
+            detections = post_processing(detections, configs)
+            detections = detections[0][1] # only first batch
+           # print(detections)
+            #######
+            ####### ID_S3_EX1-5 END #######  
+            
     ####### ID_S3_EX2 START #######     
     #######
     # Extract 3d bounding boxes from model response
     print("student task ID_S3_EX2")
     objects = [] 
 
-    ## step 1 : check whether there are any detections
-
-        ## step 2 : loop over all detections
-        
-            ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
-        
-            ## step 4 : append the current object to the 'objects' array
-        
-    #######
-    ####### ID_S3_EX2 START #######   
     
-    return objects    
+    ## step 1 : check whether there are any detections
+    for idx in range(len(detections)):
+        if len(detections[idx]) > 0:
+                            
+    ## step 2 : loop over all detections
+                _score, _x, _y, _z, _h, _w, _l, _yaw = detections[idx]
 
+    ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
+                x = (_y / configs.bev_height) * (configs.lim_x[1] - configs.lim_x[0]) + configs.lim_x[0]
+                y = (_x / configs.bev_width) * (configs.lim_y[1] - configs.lim_y[0]) + configs.lim_y[0]
+                w = (_w / configs.bev_width) * (configs.lim_y[1] - configs.lim_y[0]) 
+                l = (_l / configs.bev_height) * (configs.lim_x[1] - configs.lim_x[0])
+                z = _z + configs.lim_z[0]
+                
+    ## step 4 : append the current object to the 'objects' array
+                objects.append([1, x, y, z, _h, w, l, -_yaw])
+            
+    #######
+    ####### ID_S3_EX2 END #######   
+    return objects   
--- student/objdet_eval.py
+++ student/objdet_eval.py
@@ -39,27 +39,49 @@ def measure_detection_performance(detections, labels, labels_valid, min_iou=0.5)
     center_devs = []
     ious = []
     for label, valid in zip(labels, labels_valid):
+       
         matches_lab_det = []
         if valid: # exclude all labels from statistics which are not considered valid
-            
+
             # compute intersection over union (iou) and distance between centers
 
             ####### ID_S4_EX1 START #######     
             #######
             print("student task ID_S4_EX1 ")
 
-            ## step 1 : extract the four corners of the current label bounding-box
+            # step 1 : extract the four corners of the current label bounding-box
+            box_corners = tools.compute_box_corners(label.box.center_x, label.box.center_y, label.box.width, label.box.length, label.box.heading)
+            
             
             ## step 2 : loop over all detected objects
+            for det in detections:
 
-                ## step 3 : extract the four corners of the current detection
+                ## step 3 : extract the four corners of the current detection (use tools.compute_box_corners)
+                _score, _x, _y, _z, _h, _w, _l, _yaw = det
+               
+                det_corners = tools.compute_box_corners(_x, _y, _w, _l, _yaw)
                 
-                ## step 4 : computer the center distance between label and detection bounding-box in x, y, and z
+                ## step 4 : compute the center distance between label and detection bounding-box in x, y, and z
+                distance_center = np.array([label.box.center_x, label.box.center_y, label.box.center_z]) - np.array([_x, _y, _z])
+               
                 
                 ## step 5 : compute the intersection over union (IOU) between label and detection bounding-box
+                box_poly = Polygon(box_corners)
+                det_poly = Polygon(det_corners)
+                intersection=box_poly.intersection(det_poly).area
+                union=box_poly.union(det_poly).area
+                iou=intersection/union
                 
+                
+               
                 ## step 6 : if IOU exceeds min_iou threshold, store [iou,dist_x, dist_y, dist_z] in matches_lab_det and increase the TP count
-                
+                if iou > min_iou:
+                   # dist_x, dist_y, dist_z = distance_center
+                    dist_x, dist_y, dist_z = distance_center
+                    matches_lab_det.append([iou, dist_x, dist_y, dist_z])
+                    true_positives += 1
+                   
+                    
             #######
             ####### ID_S4_EX1 END #######     
             
@@ -68,7 +90,8 @@ def measure_detection_performance(detections, labels, labels_valid, min_iou=0.5)
             best_match = max(matches_lab_det,key=itemgetter(1)) # retrieve entry with max iou in case of multiple candidates   
             ious.append(best_match[0])
             center_devs.append(best_match[1:])
-
+            print(ious)
+            print(center_devs)
 
     ####### ID_S4_EX2 START #######     
     #######
@@ -77,25 +100,26 @@ def measure_detection_performance(detections, labels, labels_valid, min_iou=0.5)
     # compute positives and negatives for precision/recall
     
     ## step 1 : compute the total number of positives present in the scene
-    all_positives = 0
-
+    all_positives = labels_valid.sum()
+    
     ## step 2 : compute the number of false negatives
-    false_negatives = 0
-
+    
+    false_negatives = all_positives - true_positives
+    
     ## step 3 : compute the number of false positives
-    false_positives = 0
+    false_positives = len(detections) - true_positives
     
     #######
     ####### ID_S4_EX2 END #######     
     
     pos_negs = [all_positives, true_positives, false_negatives, false_positives]
     det_performance = [ious, center_devs, pos_negs]
-    
+    print(det_performance)
     return det_performance
 
 
 # evaluate object detection performance based on all frames
-def compute_performance_stats(det_performance_all):
+def compute_performance_stats(det_performance_all, configs_det):
 
     # extract elements
     ious = []
@@ -105,19 +129,29 @@ def compute_performance_stats(det_performance_all):
         ious.append(item[0])
         center_devs.append(item[1])
         pos_negs.append(item[2])
-    
+         
     ####### ID_S4_EX3 START #######     
     #######    
     print('student task ID_S4_EX3')
 
     ## step 1 : extract the total number of positives, true positives, false negatives and false positives
-    
+    pos_negs_arr = np.asarray(pos_negs)
+    positives = sum(pos_negs_arr[:,0])
+    true_positives = sum(pos_negs_arr[:,1])
+    false_negatives = sum(pos_negs_arr[:,2])
+    false_positives = sum(pos_negs_arr[:,3])
     ## step 2 : compute precision
-    precision = 0.0
+
+    precision = true_positives / (true_positives + false_positives) #
 
     ## step 3 : compute recall 
-    recall = 0.0
-
+    recall = true_positives / (true_positives + false_negatives) 
+    
+    
+ 
+   
+    
+    
     #######    
     ####### ID_S4_EX3 END #######     
     print('precision = ' + str(precision) + ", recall = " + str(recall))   
@@ -169,4 +203,3 @@ def compute_performance_stats(det_performance_all):
                     verticalalignment='top', bbox=props)
     plt.tight_layout()
     plt.show()
-
